ee69eb66c05e9a0f70d39b4bb0e5d118de860a07
jsobie3 <jakub.sobieraj01@gmail.com>
2023-01-22 19:25:36 -0600

    new repo test

---
+++ portfolio/src/data/data.js
@@ -0,0 +1,56 @@
+import Cocktail from '../assets/projects/cocktailwebapp.png'
+import Gifthub from '../assets/projects/gifthub.png'
+import Gamedeal from '../assets/projects/gamedeal.png'
+import Worddaytracker from '../assets/projects/workdaytracker.png'
+import Ecommerecebackend from '../assets/projects/ecommerecebackend.png'
+import Teamprofilegenerator from '../assets/projects/teamprofilegenerator.png'
+
+
+
+export const data=[
+    {
+        id:1,
+        name:"Vanilla JS and CSS",
+        image:Gamedeal,
+        github:"https://github.com/jsobie3/GameDeal-Lookup",
+        live:"https://project1teamthree.github.io/p1t3-GamingGrandpas/",
+        
+    },
+    {
+        id:2,
+        name:"HandleBars JS and Foundation CSS",
+        image:Cocktail,
+        github:"https://github.com/jsobie3/Cocktail-App",
+        live:"https://project2thebestmen.herokuapp.com/",
+    },
+    {
+        id:3,
+        name:"React JS and React Bootstrap",
+        image:Gifthub,
+        github:"https://github.com/jsobie3/Santas-Little-Helpers",
+        live:"https://p3-gift-hub.herokuapp.com/",
+    },
+    {
+        id:4,
+        name:"First JS project",
+        image:Worddaytracker,
+        github:"https://github.com/jsobie3/WorkDay-App-2.0",
+        live:"https://jsobie3.github.io/WorkDay-App-2.0/",
+    },
+    {
+        id:5,
+        name:"API Routes",
+        image:Ecommerecebackend,
+        github:"https://github.com/jsobie3/Object-Relational-Mapping-ORM-E-Commerce-Back-End",
+        live:"https://www.youtube.com/watch?v=8Fh7lLPejAw",
+    },
+    {
+        id:6,
+        name:"Backend node.js",
+        image:Teamprofilegenerator,
+        github:"https://github.com/jsobie3/Team-Profile-Generator",
+        live:"https://www.youtube.com/watch?v=hti3ibxN4HY",
+    },
+
+
+]
